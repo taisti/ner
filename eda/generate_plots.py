d7ce6d9470775e409fff6d14ac04852a7118f37f
Maciej Pawłowski <mac.pawlowski19@gmail.com>
2022-02-09 20:15:02 +0100

    PEP8, less than 80 chars in a single line

--- eda/generate_plots.py
+++ eda/generate_plots.py
@@ -7,7 +7,28 @@ import seaborn as sns
 import spacy
 
 from src.prepare_data_utils import process_ann_file
-from src.mappings import ENTITIES_MAP
+
+
+ENTITIES_MAP = {
+    "food_product_with_unit": "FOOD",
+    "food_product_without_unit_uncountable": "FOOD",
+    "food_product_whole": "FOOD",
+    "food_product_without_unit_countable": "FOOD",
+    "quantity": "QUANT",
+    "unit": "UNIT",
+    "physical_quality": "O",
+    "color": "O",
+    "trade_name": "O",
+    "purpose": "O",
+    "taste": "O",
+    "process": "O",
+    "example": "O",
+    "part": "O",
+    "diet": "O",
+    "possible_substitute": "O",
+    "excluded": "O",
+    "exclusive": "O"
+}
 
 
 def count_entities_types(quantities_dict, entities, nlp, entity_type):
@@ -60,10 +81,11 @@ def set_plotting_style():
     sns.set_style("whitegrid")
 
 
-def plot_entities_quantities(quantities, num_of_categories, images_folder_path, fig_title, save_fig,
-                             show_fig):
+def plot_entities_quantities(quantities, num_of_categories, images_folder_path,
+                             fig_title, save_fig, show_fig):
 
-    quantities = dict(sorted(quantities.items(), key=lambda x: x[1], reverse=True))
+    quantities = dict(
+        sorted(quantities.items(), key=lambda x: x[1], reverse=True))
 
     other_quantity = 0
 
@@ -85,17 +107,21 @@ def plot_entities_quantities(quantities, num_of_categories, images_folder_path, 
 
     # shorten labels for visualization
     for idx in range(len(entities)):
-        entities[idx] = entities[idx].replace("food product without unit uncountable",
-                                              "food product without\nunit uncountable")
-        entities[idx] = entities[idx].replace("food product with unit", "food product\nwith unit")
-        entities[idx] = entities[idx].replace("food product without unit countable",
-                                              "food product without\nunit countable")
+        entities[idx] = entities[idx].replace(
+            "food product without unit uncountable",
+            "food product without\nunit uncountable")
+        entities[idx] = entities[idx].replace("food product with unit",
+                                              "food product\nwith unit")
+        entities[idx] = entities[idx].replace(
+            "food product without unit countable",
+            "food product without\nunit countable")
 
     ax.bar(entities, quantities.values())
     plt.xticks(rotation=90, size=10)
 
     for i, v in enumerate(quantities.values()):
-        ax.text(i, v + 10, str(v), color='blue', fontweight='bold', rotation=0, ha='center', size=10)
+        ax.text(i, v + 10, str(v), color='blue', fontweight='bold', rotation=0,
+                ha='center', size=10)
 
     plt.title(fig_title, size=20)
     plt.ylim(0, 1.2 * max(quantities.values()))
@@ -109,8 +135,9 @@ def plot_entities_quantities(quantities, num_of_categories, images_folder_path, 
         plt.show()
 
 
-def plot_entities_examples_quantities(quantities, num_of_files, num_of_categories, entity_type, images_folder_path,
-                                      save_fig, show_fig):
+def plot_entities_examples_quantities(
+        quantities, num_of_files, num_of_categories, entity_type,
+        images_folder_path, save_fig, show_fig):
 
     quantities = dict(sorted(quantities.items(), key=lambda x: x[1],
                              reverse=True))
@@ -137,15 +164,17 @@ def plot_entities_examples_quantities(quantities, num_of_files, num_of_categorie
     plt.xticks(rotation=90, size=10)
 
     for i, v in enumerate(quantities.values()):
-        ax.text(i, v + max(quantities.values())*0.005, str(v), color='blue', fontweight='bold', rotation=0,
-                ha='center', size=10)
+        ax.text(i, v + max(quantities.values())*0.005, str(v), color='blue',
+                fontweight='bold', rotation=0, ha='center', size=10)
 
-    plt.title(f"{entity_type} entities quantities within {num_of_files} recipes", size=20)
+    plt.title(f"{entity_type} entities quantities within {num_of_files} "
+              f"recipes", size=20)
     plt.ylim(0, 1.2 * max(quantities.values()))
     plt.tight_layout()
 
     if save_fig:
-        fig_path = os.path.join(images_folder_path, f"{entity_type}_examples_distribution.png")
+        fig_path = os.path.join(images_folder_path,
+                                f"{entity_type}_examples_distribution.png")
         plt.savefig(fig_path)
 
     if show_fig:
@@ -159,11 +188,15 @@ def main(args):
 
     nlp = spacy.load("en_core_web_sm")
 
-    all_ann_files_paths = [os.path.join(args.annotations_folder, file) for file in os.listdir(args.annotations_folder)
+    all_ann_files_paths = [os.path.join(args.annotations_folder, file)
+                           for file in os.listdir(args.annotations_folder)
                            if ".ann" in file]
-    val_ann_files_paths = [path for path in all_ann_files_paths if 240 <= int(re.findall(r'\d+', path)[0]) < 300]
-    test_ann_files_paths = [path for path in all_ann_files_paths if 400 <= int(re.findall(r'\d+', path)[0]) < 500]
-    train_ann_files_paths = [path for path in all_ann_files_paths if path not in val_ann_files_paths
+    val_ann_files_paths = [path for path in all_ann_files_paths
+                           if 240 <= int(re.findall(r'\d+', path)[0]) < 300]
+    test_ann_files_paths = [path for path in all_ann_files_paths
+                            if 400 <= int(re.findall(r'\d+', path)[0]) < 500]
+    train_ann_files_paths = [path for path in all_ann_files_paths
+                             if path not in val_ann_files_paths
                              and path not in test_ann_files_paths]
 
     ann_files_paths = {
@@ -174,15 +207,20 @@ def main(args):
     }
 
     for set_type in ann_files_paths.keys():
-        quantities, num_of_files = calculate_quantities(ann_files_paths[set_type], count_entities_types, nlp, None)
-        fig_title = f"Entities quantities within {num_of_files} files - {set_type} recipes"
-        plot_entities_quantities(quantities, args.num_of_categories, args.images_folder, fig_title, args.save_fig,
+        quantities, num_of_files = calculate_quantities(
+            ann_files_paths[set_type], count_entities_types, nlp, None)
+        fig_title = f"Entities quantities within {num_of_files} " \
+                    f"files - {set_type} recipes"
+        plot_entities_quantities(quantities, args.num_of_categories,
+                                 args.images_folder, fig_title, args.save_fig,
                                  args.show_fig)
 
-    entity_types = ["food"] + [el for el in list(ENTITIES_MAP.keys()) if not el.startswith("food")]
+    entity_types = ["food"] + [el for el in list(ENTITIES_MAP.keys())
+                               if not el.startswith("food")]
     for entity_type in entity_types:
 
-        quantities, num_of_files = calculate_quantities(ann_files_paths['all'], count_entities_examples,
+        quantities, num_of_files = calculate_quantities(ann_files_paths['all'],
+                                                        count_entities_examples,
                                                         nlp, entity_type)
 
         if entity_type == "quantity":
@@ -195,20 +233,26 @@ def main(args):
 
             quantities = quantities_with_numeric
 
-        plot_entities_examples_quantities(quantities, num_of_files, args.num_of_categories, entity_type,
-                                          args.images_folder, args.save_fig, args.show_fig)
+        plot_entities_examples_quantities(quantities, num_of_files,
+                                          args.num_of_categories, entity_type,
+                                          args.images_folder, args.save_fig,
+                                          args.show_fig)
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-af', '--annotations-folder', type=str, default="../data/annotations",
+    parser.add_argument('-af', '--annotations-folder', type=str,
+                        default="../data/annotations",
                         help='Path to folder with annotations')
-    parser.add_argument('-if', '--images-folder', type=str, default="./images", help='Path where images are saved')
+    parser.add_argument('-if', '--images-folder', type=str, default="./images",
+                        help='Path where images are saved')
     parser.add_argument('-noc', '--num-of-categories', type=int, required=True,
                         help='Number of categories to be included')
-    parser.add_argument('--show-fig', type=bool, default=True, help='Whether to show figs')
-    parser.add_argument('--save-fig', type=bool, default=True, help='Whether to save figs')
+    parser.add_argument('--show-fig', type=bool, default=True,
+                        help='Whether to show figs')
+    parser.add_argument('--save-fig', type=bool, default=True,
+                        help='Whether to save figs')
 
     args = parser.parse_args()
 
--- src/model.py
+++ src/model.py
@@ -3,9 +3,11 @@ import re
 import torch
 import json
 
-from transformers import (AutoTokenizer, AutoModelForTokenClassification, DataCollatorForTokenClassification, Trainer,
+from transformers import (AutoTokenizer, AutoModelForTokenClassification,
+                          DataCollatorForTokenClassification, Trainer,
                           TrainingArguments)
-from utils import tokenize_and_align_labels, compute_metrics, token_to_entity_predictions
+from utils import (tokenize_and_align_labels, compute_metrics,
+                   token_to_entity_predictions)
 from datasets import Dataset
 from nervaluate import Evaluator
 
@@ -27,19 +29,22 @@ class NERTaisti:
         else:
             raise TypeError(f"{config} is not a config!")
 
-        possible_model_path = os.path.join(path_to_config_dir, self.config["model_pretrained_path"])
-        if self.config["model_pretrained_path"] and os.path.exists(os.path.join(possible_model_path,
-                                                                                "pytorch_model.bin")):
+        possible_model_path = os.path.join(path_to_config_dir,
+                                           self.config["model_pretrained_path"])
+        if self.config["model_pretrained_path"] and os.path.exists(
+                os.path.join(possible_model_path, "pytorch_model.bin")):
             model_name_or_path = possible_model_path
-            print(f"Loaded pretrained model from {os.path.abspath(model_name_or_path)}!!!")
+            print(f"Loaded pretrained model from "
+                  f"{os.path.abspath(model_name_or_path)}!!!")
         else:
-            model_name_or_path = self.config["_name_or_path"]  # huggingface's variable name for model type
+            model_name_or_path = self.config["_name_or_path"]
             print(f"Loaded huggingface checkpoint: {model_name_or_path}")
 
-        if self.config["model_pretrained_path"] and os.path.exists(os.path.join(possible_model_path,
-                                                                                "tokenizer_config.json")):
+        if self.config["model_pretrained_path"] and os.path.exists(
+                os.path.join(possible_model_path, "tokenizer_config.json")):
             tokenizer_name_or_path = possible_model_path
-            print(f"Loaded tokenizer from {os.path.abspath(tokenizer_name_or_path)}!!!")
+            print(f"Loaded tokenizer from "
+                  f"{os.path.abspath(tokenizer_name_or_path)}!!!")
         else:
             tokenizer_name_or_path = self.config["_name_or_path"]
             print(f"Loaded huggingface tokenizer: {model_name_or_path}")
@@ -50,7 +55,9 @@ class NERTaisti:
         id2label = {v: k for k, v in label2id.items()}
         torch.manual_seed(self.config["training_args"]["seed"])
         model = AutoModelForTokenClassification.from_pretrained(
-            model_name_or_path, num_labels=len(self.config["label2id"]), ignore_mismatched_sizes=True,
+            model_name_or_path,
+            num_labels=len(self.config["label2id"]),
+            ignore_mismatched_sizes=True,
             label2id=label2id, id2label=id2label
         )
 
@@ -59,7 +66,9 @@ class NERTaisti:
         )
 
         data_collator = DataCollatorForTokenClassification(
-            tokenizer=self.tokenizer, max_length=self.config["num_of_tokens"], padding="max_length"
+            tokenizer=self.tokenizer,
+            max_length=self.config["num_of_tokens"],
+            padding="max_length"
         )
 
         self.trainer = Trainer(
@@ -122,10 +131,14 @@ class NERTaisti:
 
         for recipe_idx in range(num_of_recipes):
             text_split_words = recipes[recipe_idx]
-            text_split_tokens = self.tokenizer.convert_ids_to_tokens(data["input_ids"][recipe_idx])
+            text_split_tokens = self.tokenizer.convert_ids_to_tokens(
+                data["input_ids"][recipe_idx])
 
             pred_entities.append(token_to_entity_predictions(
-                text_split_words, text_split_tokens, token_labels[recipe_idx], self.trainer.model.config.id2label
+                text_split_words,
+                text_split_tokens,
+                token_labels[recipe_idx],
+                self.trainer.model.config.id2label
             ))
 
         return pred_entities
@@ -143,12 +156,14 @@ class NERTaisti:
         return data, dataset
 
     def save_model(self):
-        save_dir = self.config["save_dir"] if self.config["save_dir"] else "taisti_ner_model"
+        save_dir = self.config["save_dir"] if self.config["save_dir"]\
+            else "taisti_ner_model"
         os.makedirs(save_dir, exist_ok=True)
 
         # Add custom config values to the config.json
         self.trainer.model.config.num_of_tokens = self.config["num_of_tokens"]
-        self.trainer.model.config.only_first_token = self.config["only_first_token"]
+        self.trainer.model.config.only_first_token = \
+            self.config["only_first_token"]
         self.trainer.model.config.training_args = self.config["training_args"]
         self.trainer.model.config.model_pretrained_path = "."
 
