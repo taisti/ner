334000fc7e7238e758ea7d6d8179dde36bccdff4
Maciej Pawłowski <mac.pawlowski19@gmail.com>
2022-02-27 16:47:17 +0100

    Visualize more than one recipe

--- src/utils.py
+++ src/utils.py
@@ -177,8 +177,8 @@ def bio_to_biluo(bio_entities):
 
     for entity_idx in range(len(bio_entities)):
         cur_entity = bio_entities[entity_idx]
-        next_entity = bio_entities[entity_idx] if entity_idx < len(
-            bio_entities) - 1 else ""
+        next_entity = bio_entities[entity_idx + 1] if \
+            entity_idx < len(bio_entities) - 1 else ""
 
         if cur_entity.startswith("B-"):
             if next_entity.startswith("I-"):
@@ -220,27 +220,36 @@ def bio_to_span(recipe, bio_entities):
     return spans
 
 
-def visualize_prediction(recipe, entities, in_jupyter=False,
+def visualize_prediction(recipes, entities, in_jupyter=False,
                          options=DISPLACY_OPTIONS):
 
-    doc = nlp(recipe)
+    if isinstance(recipes, str):
+        recipes = [recipes]
+        entities = [entities]
 
-    if isinstance(entities[0], str):
-        if any([bool(re.match(r"[UL]-", entity)) for entity in set(entities)]):
-            spans = biluo_to_span(recipe, entities)
-        else:
-            spans = bio_to_span(recipe, entities)
-    else:
+    if isinstance(entities[0][0], str):
+        if any([bool(re.match(r"[UL]-", entity)) for entity in
+                set(entities[0])]):  # entities are in BILUO schema
+            spans = [biluo_to_span(recipe, ents) for recipe, ents in
+                     zip(recipes, entities)]
+        else:  # entities are in BIO schema
+            spans = [bio_to_span(recipe, ents) for recipe, ents in
+                     zip(recipes, entities)]
+    else:  # spans
         spans = entities
 
-    ents = []
-    for span_start, span_end, label in spans:
-        ent = doc.char_span(span_start, span_end, label=label)
-        ents.append(ent)
+    docs = []
+    for recipe, recipe_spans in zip(recipes, spans):
+        doc = nlp(recipe)
+        ents = []
+        for span_start, span_end, label in recipe_spans:
+            ent = doc.char_span(span_start, span_end, label=label)
+            ents.append(ent)
 
-    doc.ents = ents
+        doc.ents = ents
+        docs.append(doc)
 
     if in_jupyter:
-        displacy.render(doc, style="ent", jupyter=True, options=options)
+        displacy.render(docs, style="ent", jupyter=True, options=options)
     else:
-        displacy.server(doc, style="ent", options=options)
+        displacy.serve(docs, style="ent", options=options)
